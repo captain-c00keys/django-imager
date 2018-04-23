29c597979ac29b3b42cccd517193d507c9565730
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-23 14:43:08 -0700

    config activate and settings

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '08*z5wglp52o)=25c#u9f6p#cuk25j^h(07cq29&blts%%#xxl'
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
