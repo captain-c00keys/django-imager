d3c5b85e4069c3ac659fbeb8da6b400ac699bcff
Peter Kim <seattlechem@yahoo.com>
2018-04-26 14:51:09 -0700

    imager_profile/views.py added

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+"""Image Profile Views."""
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from imager_images.models import Album, Photo
+from .models import ImagerProfile
+
+
+def profile_view(request, username=None):
+    """Render codes for profile."""
+    owner = False
+
+    if not username:
+        username = request.user.get_username()
+        owner = True
+        if username == '':
+            return redirect('home')
+
+    profile = get_object_or_404(ImagerProfile, user__username=username)
+    albums = Album.objects.filter(user__username=username)
+    photos = Photo.objects.filter(album__user__username=username)
+
+    if not owner:
+        photos = Photo.objects.filter(published='PUBLIC')
+        albums = Album.objects.filter(published='PUBLIC')
+
+    context = {
+        'profile': profile,
+        'albums': albums,
+        'photos': photos
+    }
+
+    return render(request, 'imager_profile/profile.html', context)
