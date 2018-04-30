40756ca3f0d73141845640209f483334e91fb85e
Peter Kim <seattlechem@yahoo.com>
2018-04-30 15:05:18 -0700

    image css deleted / thumnail photo view and detail view complete

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,14 +1,12 @@
 """Image Urls."""
 
 from django.urls import path
-\
-from .views import image_view, image_photo_view, image_one_photo, \
-                   library_view, photo_detail_view, photo_view
+from .views import image_view, image_photo_view, library_view, photo_detail_view, photo_view
 
 
 urlpatterns = [
     path('', image_view, name='image'),
     path('photos', photo_view, name='photo'),
-    path('photos/<int:photo_id>', photo_detail_view, name='photo_detail'),
+    path('photos/<int:id>', photo_detail_view, name='photo_detail'),
     path('library', library_view, name='library')
 ]
