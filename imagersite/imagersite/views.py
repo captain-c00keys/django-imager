7a005e7681a2521115a392447d8ec8f10e7a998a
Peter Kim <seattlechem@yahoo.com>
2018-05-08 14:47:28 -0700

    HomeView complete

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -3,23 +3,30 @@
 from django.shortcuts import render
 from imager_images.models import Photo
 from random import choice
+from django.views.generic import TemplateView
 
 
-def home_view(request):
-    """View is for homepage."""
-    photos = Photo.objects.all()
-    photos = photos.filter(published='PUBLIC')
-    if len(photos) > 0:
-        rand_pick = choice(photos)
-        pic_path = rand_pick.image.url
+class HomeView(TemplateView):
+    """Render a homepage."""
 
-    else:
-        pic_path = None
+    template_name = 'generic/home.html'
 
-    context = {
-        'message': 'Hello World',
-        'photos': photos,
-        'pic_path': pic_path
-    }
+    def get_context_data(self, **kwargs):
+        """Obtain photo context."""
+        context = super().get_context_data(**kwargs)
+        photos = Photo.objects.filter(published='PUBLIC')
 
-    return render(request, 'generic/home.html', context)
+        if photos.count():
+            image = photos.order_by('?').first()
+            pic_path = image.image.url
+            image_title = image.title
+
+        else:
+            pic_path = 'http://via.placeholder.com/250x250'
+            image_title = 'Placeholder'
+
+        context['pic_path'] = pic_path
+        context['image_title'] = image_title
+        context['message'] = 'Hello World'
+
+        return context
