9e5dff985fe363bff0c983e8db6c8138d310b5b3
Peter Kim <seattlechem@yahoo.com>
2018-05-09 17:45:31 -0700

    ProfileView refactored to pass username in self.kwargs

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -7,43 +7,6 @@ from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
 
-# def profile_view(request, username=None):
-#     """Render codes for profile."""
-#     owner = False
-
-#     if not username:
-#         username = request.user.get_username()
-#         owner = True
-#         if username == '':
-#             return redirect('home')
-
-#     # import pdb; pdb.set_trace()
-#     profile = get_object_or_404(ImagerProfile, user__username=username)
-
-#     albums = Album.objects.filter(user__username=username)
-#     albums_public = albums.filter(published='PUBLIC')
-#     albums_private = albums.filter(published='PRIVATE')
-
-#     photos = Photo.objects.filter(user__username=username)
-#     photos_public = photos.filter(published='PUBLIC')
-#     photos_private = photos.filter(published='PRIVATE')
-
-#     if not owner:
-#         photos_public = Photo.objects.filter(published='PUBLIC')
-#         photos_private = 'None'
-#         albums_public = Album.objects.filter(published='PUBLIC')
-#         albums_private = 'None'
-
-#     context = {
-#         'profile': profile,
-#         'photos_public': photos_public,
-#         'photos_private': photos_private,
-#         'albums_public': albums_public,
-#         'albums_private': albums_private,
-#     }
-
-#     return render(request, 'imager_profile/profile.html', context)
-  
 class ProfileView(DetailView):
     """Render a profile page."""
 
@@ -53,46 +16,18 @@ class ProfileView(DetailView):
     slug_field = 'user__username'
     model = ImagerProfile
 
-    # def get(self, *args, **kwargs):
-    #     """Retrieve keyword args."""
-    #     if not self.request.user.is_authenticated:
-    #         return redirect('home')
+    def get(self, *args, **kwargs):
+        """Retrieve keyword args."""
+        if 'username' not in kwargs:
+            self.kwargs['username'] = self.request.user.username
 
-    #     return super().get(*args, **kwargs)
-
-    # def get_queryset(self):
-    #     """Filter object."""
-    #     import pdb; pdb.set_trace()
-    #     owner = False
-
-    #     username = self.request.user.username
-    #     if username:
-    #         owner = True
-
-    #     photos = Photo.objects.filter(user__username=self.request.user.username)
-    #     photos_public = photos.objects.filter(published='PUBLIC')
-    #     photos_private = photos.objects.filter(published='PRIVATE')
-
-    #     albums = Album.objects.filter(user__username=self.request.user.username)
-    #     albums_public = albums.objects.filter(published='PUBLIC')
-    #     albums_private = albums.objects.filter(published='PRIVATE')
-    #     # import pdb; pdb.set_trace()
-
-    #     if not owner:
-    #         photos_public = Photo.objects.filter(published='PUBLIC')
-    #         photos_private = 'None'
-    #         albums_public = Album.objects.filter(published='PUBLIC')
-    #         albums_private = 'None'
-
-    #     return photos_public, photos_private, albums_public, albums_private
+        # import pdb; pdb.set_trace()
+        return super().get(*args, **kwargs) 
 
     def get_context_data(self, **kwargs):
         """Obtain profile context."""
-
         context = super().get_context_data(**kwargs)
         username = context['profile'].user.username
-
-        # import pdb; pdb.set_trace()
 
         photos = Photo.objects.filter(user__username=username)
         photos_public = photos.filter(published='PUBLIC')
@@ -111,5 +46,4 @@ class ProfileView(DetailView):
         context['albums_public_count'] = albums_public.count()
         context['albums_private_count'] = albums_public.count()
 
-        # del context['profile']
         return context
